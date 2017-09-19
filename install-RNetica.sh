32097520d32783e9800d375e7968e72f6173c66a
egouldo <elisebgould@gmail.com>
2017-09-19 12:45:03 +1000

    move install RNetica to it's own executable bash script

---
+++ install-RNetica.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -ex
+wget https://pluto.coe.fsu.edu/svn/common/RNetica/releases/RNetica_0.4-6.tar.gz
+tar -xvf 'RNetica_0.4-6.tar.gz'
+R CMD INSTALL RNetica --configure-args='--with-netica=/home/travis/build/egouldo/GrasslandAllocatr/src/Netica_API_504'
+rm -r ./RNetica/
